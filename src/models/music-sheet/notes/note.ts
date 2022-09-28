a8530289a95aea98eb8e14e77528ef88f7b8875a
Yi Zhang <yi.zhang9206@gmail.com>
2022-09-28 00:13:52 -0400

    added accidental on note class

--- src/models/music-sheet/notes/note.ts
+++ src/models/music-sheet/notes/note.ts
@@ -1,15 +1,18 @@
 import type { NoteName } from '@/enums/note-name.enum';
 import type { OctaveRegister } from '@/enums/octave-register.enum';
+import type { Accidental } from '@/enums/accidental.enum';
 import type { NoteValue } from '@/enums/note-value.enum';
 
 export abstract class Note {
     public name: NoteName;
     public octave: OctaveRegister;
+    public accidental: Accidental;
     public value: NoteValue;
 
-    constructor(name: NoteName, octave: OctaveRegister, value: NoteValue) {
+    constructor(name: NoteName, octave: OctaveRegister, accidental: Accidental, value: NoteValue) {
         this.name = name;
         this.octave = octave;
+        this.accidental = accidental;
         this.value = value;
     }
 }
--- src/models/music-sheet/notes/quarter-note.ts
+++ src/models/music-sheet/notes/quarter-note.ts
@@ -1,12 +1,13 @@
 import type { NoteName } from '@/enums/note-name.enum';
 import type { OctaveRegister } from '@/enums/octave-register.enum';
+import { Accidental } from '@/enums/accidental.enum';
 import { NoteValue } from '@/enums/note-value.enum';
 
 import { Note } from './note';
 
 export class QuarterNote extends Note {
 
-    constructor(name: NoteName, octave: OctaveRegister) {
-        super(name, octave, NoteValue.Quarter);
+    constructor(name: NoteName, octave: OctaveRegister, accidental = Accidental.None) {
+        super(name, octave, accidental, NoteValue.Quarter);
     }
 }
